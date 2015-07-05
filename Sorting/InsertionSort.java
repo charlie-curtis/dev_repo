59cc726441f7e3b5fbbe995806cdffce146a0718
Charlie Curtis <charlie-curtis@outlook.com>
2015-07-05 03:19:48 -0400

    refactored InsertionSort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -4,20 +4,11 @@ public class InsertionSort extends Sorting {
 	public static void sort(int[] array){
 	 
 		for(int i = 1; i<array.length; i++){
-			
-			for(int j = i-1; j>=0; j--){
-				if(array[j]>array[i]){
-					int temp = array[j];
-					array[j] = array[i];
-					array[i] = temp;
-					i--;
-					j++;
-				}
-				else{
-					continue;
-				}
+			int temp = array[i];
+			for(int j = i-1; j>=0 && array[j] > temp; j--){
+					array[i] = array[j];
+					array[j] = temp;
 			}
-			
 		}
 	}
 	
