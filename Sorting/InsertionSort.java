c335b33f02efa4c077fae0d11b7fa42cdf87f46f
Charlie Curtis <charlie-curtis@outlook.com>
2015-07-05 03:11:19 -0400

    Add SelectionSort, modified InsertionSort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -3,25 +3,30 @@ public class InsertionSort extends Sorting {
 	//java operates on the object themselves, so you don't have to return the array
 	public static void sort(int[] array){
 	 
-		for(int i = 0; i<array.length-1; i++){
-			int startingIndex = array[i];
-			for(int j = i+1; j<array.length; j++){
-				if(array[i]>array[j]){
-					int temp1 = array[j];
+		for(int i = 1; i<array.length; i++){
+			
+			for(int j = i-1; j>=0; j--){
+				if(array[j]>array[i]){
+					int temp = array[j];
 					array[j] = array[i];
-					array[i] = temp1;
+					array[i] = temp;
+					i--;
+					j++;
 				}
 				else{
 					continue;
 				}
 			}
+			
 		}
 	}
 	
 	public static void main(String[] args) throws Exception{
 
-		int[] array1 = {3,7,9,4,2,0, 32, 90, 12,32,19,138,2093,3,3,3,4,29,10,-8,-42,17};
+		int[] array1 = {7,3,9,4,2,0,13,28,109,-3,-10,-17,-39};
 		sort(array1);
+		
+		System.out.println();
 		if(isSorted(array1)){
 			System.out.println("I am sorted");
 		}
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,36 @@
+public class SelectionSort extends Sorting {
+
+	//java operates on the object themselves, so you don't have to return the array
+	public static void sort(int[] array){
+	 
+		for(int i = 0; i<array.length-1; i++){
+			int minValue = array[i];
+			int replacementIndex = i;
+			for(int j = i+1; j<array.length; j++){
+				if(minValue>array[j]){
+					minValue = array[j];
+					replacementIndex=j;
+				}
+				else{
+					continue;
+				}
+			}
+			int temp = array[i];
+			array[i] = minValue;
+			array[replacementIndex] = temp;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+
+		int[] array1 = {11,7,9,4,2,0};
+		sort(array1);
+		if(isSorted(array1)){
+			System.out.println("I am sorted");
+		}
+		else{
+			System.out.println("I am NOT sorted");
+		}
+	}
+	
+}
