237ecf5d800c089ff8253de3b5ac7300575eefc5
Charlie Curtis <charlie-curtis@outlook.com>
2015-07-16 23:25:33 -0400

    implemented a bitwise solution; its elegant but still cant handle the leetcode inputs

--- leetcode/leetcode_number-of-1-bits/SolutionTester.java
+++ leetcode/leetcode_number-of-1-bits/SolutionTester.java
@@ -5,6 +5,10 @@ public class SolutionTester{
 
 		Solution sol = new Solution();
   		System.out.println(sol.hammingWeight(67));
+  		Solution_v1 test_v1 = new Solution_v1();
+  		System.out.println(test_v1.hammingWeight(32823));
+
+  		   //(01000000000000000000000000000000)
 
 	}
 } 
---
+++ leetcode/leetcode_number-of-1-bits/Solution_v1.java
@@ -0,0 +1,14 @@
+public class Solution_v1{
+
+	public int hammingWeight(int n) {
+		int count = 0;
+	 	for(int i = 1; i<=n; i*=2){
+	 		System.out.println("i is now " + i);
+	 		if((i & n) != 0){
+	 			count++;
+	 		}
+	 	}
+	 	return count;
+	}
+}
+
