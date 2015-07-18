8c883a8a092908f8550397cefb7dd4d9a170aa88
Charlie Curtis <charlie-curtis@outlook.com>
2015-07-17 22:56:38 -0400

    add run file. working on add list. correct output but 2slow

--- leetcode/NODE-PROBLEMS/ListNode.java
+++ leetcode/NODE-PROBLEMS/ListNode.java
@@ -1,30 +1,68 @@
-public class ListNode{
+public class ListNode
+{
 
-	public ListNode(int x){
+	int val;
+	ListNode next;
+	public ListNode(int x) {
 		this.val = x;
 	}
 
-	int val;
-	public ListNode next;
-	
-	public ListNode reverseList(ListNode head) {
+	/**This method takes in a linkedlist and reverses it*/
+	ListNode reverseList(ListNode head) {
 
-	if(head == null || head.next ==null){
-	    return head;
+		ListNode setPrevious = null;
+		ListNode setNext = null;
+
+		if(head == null || head.next ==null) {
+		    return head;
+		}
+
+		while(head.next != null) {    
+		    setNext = head.next;
+		    head.next = setPrevious;
+		    setPrevious = head;
+		    head=setNext;
+		}
+		//edge case
+		head.next = setPrevious;
+		return head;
 	}
 
-	ListNode setPrevious = null;
-	ListNode setNext = null;
-	while(head.next != null){    
-	    setNext = head.next;
-	    System.out.println("setNext: " + setNext.toString());
-	    head.next = setPrevious;
-	    setPrevious = head;
-	    head=setNext;
-	}
-	System.out.println(head.toString());
-	//edge case. the last element becomes the first element
-	head.next = setPrevious;
-	return head;
-	}
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    
+        int carryOver = 0, num1 = 0, num2 = 0, result = 0;
+        ListNode returnMe = new ListNode(0);
+        ListNode head = returnMe;
+        
+        while(l1 != null || l2 != null || carryOver == 1){
+            
+            ListNode temp = new ListNode(0);
+            num1 = l1 == null? 0 : l1.val;
+            num2 = l2 == null? 0 : l2.val;
+            result = num1+num2+carryOver;
+            System.out.println("result is " + result);
+            
+            if(result >= 10){
+                System.out.println("carry 1 ran");
+                carryOver = 1;
+            }else{
+                System.out.println("Carry 0 ran");
+                carryOver = 0;
+            }
+            
+            System.out.println("val is: " + result % 10);
+            returnMe.val = result % 10;
+            l1=  l1 == null? l1 : l1.next;
+            l2 = l2 == null? l2 : l2.next;
+            if(l1 != null || l2 != null || carryOver ==1){
+                //we running the loop again, boyz
+                returnMe.next = temp;
+                returnMe = returnMe.next;  
+            }
+        }
+        System.out.println("head is" + head.val);
+        return head;
+    }
 }
+
--- leetcode/NODE-PROBLEMS/ListNodeTester.java
+++ leetcode/NODE-PROBLEMS/ListNodeTester.java
@@ -1,11 +1,13 @@
-public class ListNodeTester{
+public class ListNodeTester
+{
 
 
-	public static void main(String[] args){
-		test_reverseList();
+	public static void main(String[] args) { 
+		//test_reverseList();
+		test_addList();
 	}
 
-	protected static void test_reverseList(){
+	protected static void test_reverseList() {
 		
 		ListNode node1 = new ListNode(1);
 		ListNode node2 = new ListNode(2);
@@ -21,8 +23,60 @@ public class ListNodeTester{
 			System.out.println(result.val);
 			result = result.next;
 
-		}
-			
+		}		
 	}
 
+	protected static void test_addList() {
+
+		int[] testList1 = {5,5,5,2,2,3,6,3,6};
+		int[] testList2 = {5,3,4,9,7,9,2,6,9,1};
+
+		//1962979435 + 636322555
+		
+		ListNode node1_1 = new ListNode(5);
+		ListNode node1_2 = new ListNode(5);
+		ListNode node1_3 = new ListNode(5);
+		ListNode node1_4 = new ListNode(2);
+		ListNode node1_5 = new ListNode(2);
+		ListNode node1_6 = new ListNode(3);
+		ListNode node1_7 = new ListNode(6);
+		ListNode node1_8 = new ListNode(3);
+		ListNode node1_9 = new ListNode(6);
+
+		node1_1.next = node1_2;
+		node1_2.next = node1_3;
+		node1_3.next = node1_4;
+		node1_4.next = node1_5;
+		node1_5.next = node1_6;
+		node1_6.next = node1_7;
+		node1_7.next = node1_8;
+		node1_8.next = node1_9;
+
+		ListNode node2_0 = new ListNode(5);
+		ListNode node2_1 = new ListNode(3);
+		ListNode node2_2 = new ListNode(4);
+		ListNode node2_3 = new ListNode(9);
+		ListNode node2_4 = new ListNode(7);
+		ListNode node2_5 = new ListNode(9);
+		ListNode node2_6 = new ListNode(2);
+		ListNode node2_7 = new ListNode(6);
+		ListNode node2_8 = new ListNode(9);
+		ListNode node2_9 = new ListNode(1);
+
+		node2_0.next = node2_1;
+		node2_1.next = node2_2;
+		node2_2.next = node2_3;
+		node2_3.next = node2_4;
+		node2_4.next = node2_5;
+		node2_5.next = node2_6;
+		node2_6.next = node2_7;
+		node2_7.next = node2_8;
+		node2_8.next = node2_9;
+		
+		ListNode result = node1_1.addTwoNumbers(node1_1, node2_0);
+		while(result != null){
+			System.out.print(result.val + "->");
+			result = result.next;
+		}
+	}
 }
