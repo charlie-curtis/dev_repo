63cbc17be7d48fbcdc002c3758cb5dc6001ee039
Charlie Curtis <ccurtis@salesforce.com>
2015-12-14 23:22:02 -0500

    move-zeroes

---
+++ leetcode/move-zeroes/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int c = 0;
+        for (int i = 0; i < nums.length; i++) {
+            
+            if(nums[i] != 0) {
+                nums[c] = nums[i];
+                c++;
+            }    
+                
+        }
+        for (int j = c; j< nums.length; j++) {
+            nums[j] = 0;
+        }
+    }
+}
