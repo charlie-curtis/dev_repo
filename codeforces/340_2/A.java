c9b6f06f25b8d00eb5507bd0ec3bf614e7969afd
Charlie Curtis <ccurtis@salesforce.com>
2016-01-23 12:42:39 -0500

    solve problem A

---
+++ codeforces/340_2/A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class A
+{
+
+     public static void main(String[] args)
+     {
+	  Scanner s = new Scanner(System.in);
+	  System.out.println((int)Math.ceil(s.nextDouble()/5));
+     }
+
+
+
+
+
+}
